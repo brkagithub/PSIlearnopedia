058ee7d351090b095e31c477b2ecc0e12e409af1
brka <mbrkic16@gmail.com>
2022-05-21 22:23:36 +0200

    Removed extra url

--- fazaImplementacije/learnopedia/urls.py
+++ fazaImplementacije/learnopedia/urls.py
@@ -36,7 +36,6 @@ urlpatterns = [
     path('updateProfile/<int:profile_id>', updateProfile, name="updateProfile"),
     path('makequestions/<int:article_id>',makequestions, name='makequestion'),
     path('test/<int:article_id>', test, name="test"),
-    path('create_article/makequestions/<int:article_id>',makequestions, name='makequestion'),
     path('makecomment/<int:article_id>', makecomment, name='makecomment'),
     path('comment/<int:comment_id>', comment, name='comment')
 ]
